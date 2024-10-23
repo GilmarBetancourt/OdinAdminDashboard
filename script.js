dbbaead1be3ae30a73e74ca1a0c41cb78c1fb803
GilmarBetancourt <gilmarbeta@hotmail.com>
2024-10-23 16:32:30 -0400

    It's alive! Fixed the code to add books to library

--- script.js
+++ script.js
@@ -8,12 +8,10 @@ function Book(author, title, pages, read){
     this.read = read;
 }
 
-let newBook={};
-
 document.getElementById('formButton').addEventListener('click', 
     function() {
         //event.preventDefault();
-        
+       
         //get the information from the form
         const author =  document.getElementById("author").value;
         const title = document.getElementById("title").value;
@@ -31,12 +29,37 @@ document.getElementById('formButton').addEventListener('click',
 });
 
 ///here we add the books to the library
-function addBookToLibrary(){
+function addBookToLibrary(book){
+    
+    const articleDiv = document.getElementById('article');
 
-    //this is the idea display the new book in the DOM
-    const booksCards = document.getElementById('article');
+    //creating the DOM elements
     const cardDiv = document.createElement('div');
-    cardDiv.textContent = `Author: ${newBook.author}, Title: ${newBook.title}, Pages: ${newBook.pages}, Read: ${newBook.read}`;
-    booksCards.appendChild(cardDiv);
+    const bookTitle = document.createElement('h2');
+    const bookAuthor = document.createElement('h3');
+    const bookPages = document.createElement('p');
+    const bookStatus = document.createElement('p');
+
+    //adding the classes to the DOM elements
+    cardDiv.classList.add("card");
+    bookTitle.classList.add("title");
+    bookAuthor.classList.add("author");
+    bookPages.classList.add("pages");
+    bookStatus.classList.add("status");
+
+    // Setting text content for the elements
+    bookTitle.textContent = book.title; 
+    bookAuthor.textContent = book.author; 
+    bookPages.textContent = `Pages: ${book.pages}`; 
+    bookStatus.textContent = `Status: ${book.read}`; 
+
+    //appending the elements to the card
+    cardDiv.appendChild(bookTitle);
+    cardDiv.appendChild(bookAuthor);
+    cardDiv.appendChild(bookPages);
+    cardDiv.appendChild(bookStatus);
+
+    //appending the card to the article
+    articleDiv.appendChild(cardDiv);
             
 }
